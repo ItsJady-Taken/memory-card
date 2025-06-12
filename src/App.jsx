ec39c7b584db6ba989aebc804957cd0d799288b5
David <davidkhun2004@gmail.com>
2025-06-12 16:28:48 +0700

    Project Memory Card Project in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header";
+import Body from "./components/MainBody";
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 }
--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,8 +1,25 @@
-function Body({ children }) {
-  if (children) {
-    return <main>{children}</main>;
-  }
-  return;
+import { useState, useEffect } from "react";
+
+function Body() {
+  const [posts, setPosts] = useState([]);
+  const api = "https://pokeapi.co/api/v2/pokemon/riolu";
+  useEffect(() => {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Body</h1>
+      <p>{posts.name}</p>
+      <img src={posts.sprites.front_default} alt="pokemon" />
+    </div>
+  );
 }
 
 export default Body;

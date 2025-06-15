d06b73a578b06b23f46006a7007b2ccac5967de5
David <davidkhun2004@gmail.com>
2025-06-16 05:05:49 +0700

    Fix fetching api image

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -2,22 +2,35 @@ import { useState, useEffect } from "react";
 
 function Body() {
   const [posts, setPosts] = useState([]);
+
   const api = "https://pokeapi.co/api/v2/pokemon/riolu";
   useEffect(() => {
     fetch(api)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
         setPosts(data);
         console.log(data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) =>
+        console.log("There was a problem with the fetch operation: " + error)
+      );
   }, []);
 
   return (
     <div>
       <h1>Body</h1>
+
       <p>{posts.name}</p>
-      <img src={posts.sprites.front_default} alt="pokemon" />
+      <img
+        src={posts.sprites?.front_default}
+        className="App-logo"
+        alt="pokemon"
+      />
     </div>
   );
 }

da2ce0a8d70a9d10e476005e361a8707e9b9426e
David <davidkhun2004@gmail.com>
2025-06-26 16:44:43 +0700

    Fix fetching api, able to fetcth multiple api ids

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from "react";
 
 function MemoryCard() {
-  const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const handleClick = () => {
-    fetchPokemon().then((data) => {
+    fetchPokemon(8).then((data) => {
       setLoading(false);
       setPosts(data);
       console.log(data);
     });
   };
-
   useEffect(() => {
-    fetchPokemon().then((data) => {
+    fetchPokemon(8).then((data) => {
       setLoading(false);
       setPosts(data);
       console.log(data);
@@ -23,41 +22,51 @@ function MemoryCard() {
 
   return (
     <>
-      {loading ? (
-        <h1>Loading...</h1>
+      {posts.length === 0 ? (
+        <h1>Fetching...</h1>
       ) : (
-        <main>
-          <CardContainer
-            onClick={handleClick}
-            image={posts.sprites.front_default}
-            name={posts.name}
-          />
-          <CardContainer
-            onClick={handleClick}
-            image={posts.sprites.back_default}
-            name={posts.name}
-          />
+        <main className="main-body">
+          {posts.map((pokemon) => (
+            <CardContainer
+              id={pokemon.id}
+              key={pokemon.id}
+              onClick={handleClick}
+              image={pokemon.sprites.front_default}
+              name={pokemon.name}
+            />
+          ))}
         </main>
       )}
     </>
   );
 }
 
-function CardContainer({ onClick, image, name }) {
+function CardContainer({ onClick, image, name, id, keyId }) {
   return (
-    <div className="post-container" onClick={onClick}>
+    <div key={keyId} id={id} className="post-container" onClick={onClick}>
       <img src={image} className="post-image" alt="pokemon" />
       <p className="post-name">{name}</p>
     </div>
   );
 }
 
-async function fetchPokemon() {
-  const randomNumber = Math.floor(Math.random() * 18) + 1;
-  const api = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-  const response = await fetch(api);
-  const data = await response.json();
-  return data;
+async function fetchPokemon(count) {
+  const pokemonIds = [];
+
+  while (pokemonIds.length < count) {
+    const randomId = Math.floor(Math.random() * 24) + 1;
+    if (!pokemonIds.includes(randomId)) {
+      pokemonIds.push(randomId);
+    }
+  }
+  const promises = pokemonIds.map((id) =>
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) =>
+      response.json()
+    )
+  );
+
+  const pokemonlist = await Promise.all(promises);
+  return pokemonlist;
 }
 
 export default MemoryCard;
